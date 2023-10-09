73a0359326533a1751bc76c022fb175ec0f90053
Santosh1295 <chougulesantosh95@gmail.com>
2023-10-09 20:29:42 +0530

    first commit

---
+++ src/test/java/api/payload/user.java
@@ -0,0 +1,83 @@
+package api.payload;
+
+public class user {
+//POJO(Plain old Java Object) class - It is created to send payLoad as a requestBody or 
+//	request data with request to create user or update user to the server 
+
+	// first create variables
+	int id;
+	String username;
+	String firstName;
+	String lastName;
+	String email;
+	String password;
+	String phone;
+	int userStatus = 0;
+
+	// get methods to read values of this variables
+	// set methods to set values to this variables
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public int getUserStatus() {
+		return userStatus;
+	}
+
+	public void setUserStatus(int userStatus) {
+		this.userStatus = userStatus;
+	}
+
+}
